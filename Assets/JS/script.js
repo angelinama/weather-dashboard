5ded16e06f77fa52628c30d9cc88f1f1c65ab1af
Angelina Ma <angelina890308@gmail.com>
2020-07-24 15:49:20 -0700

    add error handling when city not found

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -24,7 +24,6 @@ $( document ).ready(function() {
             localStorage.setItem("cityList", JSON.stringify(cityList));
             currentWeather(localStorage.getItem("lastCity"));
             forecast5(localStorage.getItem("lastCity"));
-            refreshHistory();
         }
         
     });    
@@ -64,63 +63,74 @@ function currentWeather(city, lat, lon) {
         return -1;
     }
     
-  $.ajax({
-  url: queryURL,
-  method: "GET"
-})
-.then(function(response) {
-    //NoTE: A easier way to get current date would be from 5-day forcast but II want to use Date() for practice
-    var date = new Date(response.dt * 1000);
-    var ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
-    var mo = new Intl.DateTimeFormat('en', { month: 'numeric' }).format(date);
-    var da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
- 
-    var cardDiv = $('<div>').attr("class", "card-body"); //only purpose is for Bootstrap card styling
-    $("#cityNow").html(cardDiv); //clear out the div
-    
-    var h2 = $("<h2>").text(`${response.name} (${mo}/${da}/${ye})`);
-    var iconid = response.weather[0].icon;
-    var iconImg = `https://openweathermap.org/img/wn/${iconid}@2x.png`;
-    var img = $("<img>").attr("src", iconImg).attr("alt", response.weather[0].main); //alter is the weather text
-    h2.append(img);
-    
-    var tempF = (response.main.temp - 273.15) * 1.80 + 32; // Convert the temp to fahrenheit
-    var p1 = $("<p>").text("Temperature: " + tempF.toFixed(2));
-    var p2 = $("<p>").text("Humidity: " + response.main.humidity);
-    var p3 = $("<p>").text("Wind Speed: " + response.wind.speed);
-    cardDiv.append(h2).append(p1).append(p2).append(p3);
-
-    var lat = response.coord.lat;
-    var lon = response.coord.lon;
-
-    //get UV Index from a different API call
-    var queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`;
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        var uvIndex = response.value;
-        var p4 = $("<p>").text("UV Index: ");
-        var span = $('<span>').html(uvIndex).css({"color": "white", "padding": "4px"});
-        p4.append(span);
-        cardDiv.append(p4);
+    })
+    .then(
+        //.done() call back
+        function(response) { 
+            //NoTE: A easier way to get current date would be from 5-day forcast but II want to use Date() for practice
+            var date = new Date(response.dt * 1000);
+            var ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+            var mo = new Intl.DateTimeFormat('en', { month: 'numeric' }).format(date);
+            var da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+        
+            var cardDiv = $('<div>').attr("class", "card-body"); //only purpose is for Bootstrap card styling
+            $("#cityNow").html(cardDiv); //clear out the div
+            
+            var h2 = $("<h2>").text(`${response.name} (${mo}/${da}/${ye})`);
+            var iconid = response.weather[0].icon;
+            var iconImg = `https://openweathermap.org/img/wn/${iconid}@2x.png`;
+            var img = $("<img>").attr("src", iconImg).attr("alt", response.weather[0].main); //alter is the weather text
+            h2.append(img);
+            
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32; // Convert the temp to fahrenheit
+            var p1 = $("<p>").text("Temperature: " + tempF.toFixed(2));
+            var p2 = $("<p>").text("Humidity: " + response.main.humidity);
+            var p3 = $("<p>").text("Wind Speed: " + response.wind.speed);
+            cardDiv.append(h2).append(p1).append(p2).append(p3);
 
-        //set text color to white and background color based on Image file. E.g. <3 green
-        if (uvIndex < 3) {
-            span.css("background-color", uvColorCodes[0]);
-        } else if (uvIndex < 6) {
-            span.css("background-color", uvColorCodes[1]);
-        } else if (uvIndex < 8) {
-            span.css("background-color", uvColorCodes[2]);
-        } else if (uvIndex < 11) {
-            span.css("background-color", uvColorCodes[3]);
-        } else {
-            span.css("background-color", uvColorCodes[4]);
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+
+            //get UV Index from a different API call
+            var queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                var uvIndex = response.value;
+                var p4 = $("<p>").text("UV Index: ");
+                var span = $('<span>').html(uvIndex).css({"color": "white", "padding": "4px"});
+                p4.append(span);
+                cardDiv.append(p4);
+
+                //set text color to white and background color based on Image file. E.g. <3 green
+                if (uvIndex < 3) {
+                    span.css("background-color", uvColorCodes[0]);
+                } else if (uvIndex < 6) {
+                    span.css("background-color", uvColorCodes[1]);
+                } else if (uvIndex < 8) {
+                    span.css("background-color", uvColorCodes[2]);
+                } else if (uvIndex < 11) {
+                    span.css("background-color", uvColorCodes[3]);
+                } else {
+                    span.css("background-color", uvColorCodes[4]);
+                }  
+            });  
+        }, 
+        //.fail() call back
+        function (error) {
+            console.log(error);
+            alert("City not found! Please check your input. Tips: please refer to Google for correct city name. e.g. Washington D.C.");
+            //correct lastCity and cityList in localStorage
+            cityList = JSON.parse(localStorage.getItem("cityList"));
+            cityList.pop();
+            localStorage.setItem("cityList", JSON.stringify(cityList));
+            localStorage.setItem("lastCity", cityList[cityList.length - 1]);
         }
-       
-    });
-    
-  });
+        );
 
 }
 
@@ -137,29 +147,37 @@ function forecast5(city, lat, lon) {
         url: queryURL,
         method: "GET"
     })
-    .then(function(response) {
-        $("#forcastHeader").text("5-Day Forcast");
-        $("#forcast5").html("");
-        for (var i = 0; i < response.list.length; i+= 8) {
-            var date = response.list[i].dt_txt;
-            date = date.split(" ")[0]; //get only the date part of timestamp, e.g.2017-01-31
-            var dList = date.split("-");
-            date = dList[1] + "/" + dList[2] + "/" + dList[0];
-            
-            var h5 = $("<h5>").text(date).attr("class", "card-title");
-            var iconid = response.list[i].weather[0].icon;
-            var iconImg = `https://openweathermap.org/img/wn/${iconid}@2x.png`;
-            var img = $("<img>").attr("src", iconImg).attr("alt", response.list[i].weather[0].main).css({"width":"40px", "height":"40px" }); //alter is the weather text
+    .then(
+        function(response) {
+            $("#forcastHeader").text("5-Day Forcast");
+            $("#forcast5").html("");
+            for (var i = 0; i < response.list.length; i+= 8) {
+                var date = response.list[i].dt_txt;
+                date = date.split(" ")[0]; //get only the date part of timestamp, e.g.2017-01-31
+                var dList = date.split("-");
+                date = dList[1] + "/" + dList[2] + "/" + dList[0];
                 
-            var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32; // Convert the temp to fahrenheit
-            var p1 = $("<p>").text("Temp.: " + tempF.toFixed(2)).attr("class", "card-text");
-            var p2 = $("<p>").text("Humidity: " + response.list[i].main.humidity).attr("class", "card-text");
+                var h5 = $("<h5>").text(date).attr("class", "card-title");
+                var iconid = response.list[i].weather[0].icon;
+                var iconImg = `https://openweathermap.org/img/wn/${iconid}@2x.png`;
+                var img = $("<img>").attr("src", iconImg).attr("alt", response.list[i].weather[0].main).css({"width":"40px", "height":"40px" }); //alter is the weather text
+                    
+                var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32; // Convert the temp to fahrenheit
+                var p1 = $("<p>").text("Temp.: " + tempF.toFixed(2)).attr("class", "card-text");
+                var p2 = $("<p>").text("Humidity: " + response.list[i].main.humidity).attr("class", "card-text");
 
-            var cardEl = $("<div>").attr("class", "card card-block bg-primary text-white border p-3 .rounded-sm");
-            cardEl.append(h5).append(img).append(p1).append(p2);
-            $("#forcast5").append(cardEl);
+                var cardEl = $("<div>").attr("class", "card card-block bg-primary text-white border p-3 .rounded-sm");
+                cardEl.append(h5).append(img).append(p1).append(p2);
+                $("#forcast5").append(cardEl);
             }
-        });
+
+            refreshHistory();
+        },
+        //.fail() call back
+        function (error) {
+            console.log(error);
+        }
+        );
 }
 
 //-------------Helper functions--------------------//
